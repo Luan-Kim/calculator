9296a2bd0c03974967cde8d886b10ed2ffac8b42
Kim kyoung min <36768658+Luan-Kim@users.noreply.github.com>
2018-03-04 10:16:40 +0900

    Add files via upload
    곱샘 계산기

---
+++ 곱셈 계산기.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int num1, num2, num3, num4, num5;
+	scanf("%d%d%d%d%d", &num1, &num2, &num3, &num4, &num5);
+	printf("%d", num1 + num2 + num3 + num4 + num5);
+	printf("%d", num1 * num2 * num3 * num4 * num5);
+	return 0;
+}
